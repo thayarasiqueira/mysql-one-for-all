3cbea1a401fa4223342da3f6e367cf812c62c426
Thayara Siqueira <thay.siqu@gmail.com>
2022-07-26 16:16:24 -0300

    fix: fixing plan_id which represents correct plan type intended(req10)

--- desafio10.sql
+++ desafio10.sql
@@ -2,6 +2,6 @@ SELECT t.track_name AS nome, COUNT(p.user_id) AS reproducoes
 FROM SpotifyClone.tracks AS t
 INNER JOIN SpotifyClone.played AS p ON p.track_id = t.track_id
 INNER JOIN SpotifyClone.users AS u ON p.user_id = u.user_id
-WHERE u.plan_id IN (1, 4)
+WHERE u.plan_id IN (1, 2)
 GROUP BY nome
 ORDER BY t.track_name; 
