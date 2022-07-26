b6c6d8924eaec24fe8b387234b0f9f274f09c3e2
Thayara Siqueira <thay.siqu@gmail.com>
2022-07-26 15:58:50 -0300

    feat: getting the number of times each song was played by users who are in two specific types of plan(req10)

---
+++ desafio10.sql
@@ -0,0 +1,7 @@
+SELECT t.track_name AS nome, COUNT(p.user_id) AS reproducoes
+FROM SpotifyClone.tracks AS t
+INNER JOIN SpotifyClone.played AS p ON p.track_id = t.track_id
+INNER JOIN SpotifyClone.users AS u ON p.user_id = u.user_id
+WHERE u.plan_id IN (1, 4)
+GROUP BY nome
+ORDER BY t.track_name; 
